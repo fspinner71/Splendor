3d3347aa2c74d6ad1ada2177cff68e7d1903084c
Shawn L. <shawnlianxiang@gmail.com>
2024-11-05 09:04:50 -0600

    Update Card.java
    Created card class

--- src/Card.java
+++ src/Card.java
@@ -1,23 +1,49 @@
-import java.util.*;
 import java.awt.image.*;
-import javax.swing.*;
-import javax.imageio.ImageIO;
 
 public class Card {
-    public static BufferedImage[5] lvl1cards;
+    public static BufferedImage[] lvl1cards;
+    public static BufferedImage[] lvl2cards;
+    public static BufferedImage[] lvl3cards;
 
+    public static BufferedImage[] cardBacks;
+
+    static {
+        lvl1cards = new BufferedImage[5];
+        lvl2cards = new BufferedImage[10];
+        lvl3cards = new BufferedImage[10];
+
+        cardBacks = new BufferedImage[3];
+    }
 
 
     private int points;
-    private int gem_col;
+    private int gemColor;
     private int level;
-    private int[] price;
+    private int[] price; 
     private Button button;
+    private boolean isFlipped;
 
     public Card(int points, int gemColor, int level, int[] price){
         this.points = points;
-        this.gem_col = gemColor;
+        this.gemColor = gemColor;
         this.level = level;
         this.price = price;
+        this.isFlipped = false;
+    }
+
+    public int getPoints(){
+        return this.points;
+    }
+
+    public int getGem(){
+        return this.gemColor;
+    }
+
+    public int[] getPrice(){
+        return this.price;
+    }
+
+    public void flip(){
+        this.isFlipped = (!isFlipped) ? true : false;
     }
 }
