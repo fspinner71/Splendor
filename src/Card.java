afa8fa2e32e306516494e08605725900b97a1e37
Shawn L. <shawnlianxiang@gmail.com>
2024-11-05 09:04:24 -0600

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,23 @@
+import java.util.*;
+import java.awt.image.*;
+import javax.swing.*;
+import javax.imageio.ImageIO;
 
 public class Card {
+    public static BufferedImage[5] lvl1cards;
 
+
+
+    private int points;
+    private int gem_col;
+    private int level;
+    private int[] price;
+    private Button button;
+
+    public Card(int points, int gemColor, int level, int[] price){
+        this.points = points;
+        this.gem_col = gemColor;
+        this.level = level;
+        this.price = price;
+    }
 }
