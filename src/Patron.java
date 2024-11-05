f52db17a9f9bfc813e3cd33e802ca780951ba9a6
Shawn L. <shawnlianxiang@gmail.com>
2024-11-05 09:06:49 -0600

    Update Patron.java

--- src/Patron.java
+++ src/Patron.java
@@ -10,21 +10,26 @@ private Button button;
         images = new BufferedImage[10];
         //NEEDED CODE ONCE WE IMPORT THE IMAGES
     }
+
     public Patron(int p, int[] pr, int i, Button b) {  //points/price/integer that corresponds with the array for the image
         points = p;
         price = pr;
         image = i;
         button = b;
     }
+
     public int getPoints() {
         return points;
     }
+
     public int[] getPrice() {
         return price;
     }
+
     public Button getButton() {
         return button;
     }
+    
     public void paint() {
         //xx
     }
