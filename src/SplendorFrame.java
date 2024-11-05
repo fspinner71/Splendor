9a44248bd1c3335e34b3d53a20a3ceacf04aae4b
fidgetspinner71 <nathans.fs71@gmail.com>
2024-11-05 09:12:30 -0600

    moved enums to splendorgame

--- src/SplendorFrame.java
+++ src/SplendorFrame.java
@@ -5,13 +5,6 @@ public class SplendorFrame extends JFrame {
     private final int WIDTH = 1280;
     private final int HEIGHT = 720;
 
-    public final int BLACK = 0;
-    public final int WHITE = 1;
-    public final int GREEN = 2;
-    public final int RED = 3;
-    public final int BLUE = 4;
-    public final int YELLOW = 5;
-
     public SplendorFrame()
     {
         //Setting basic settings of the window
--- src/SplendorGame.java
+++ src/SplendorGame.java
@@ -4,11 +4,26 @@ import java.awt.event.MouseListener;
 import javax.swing.*;
 
 public class SplendorGame extends JPanel implements MouseListener{
+    public final int BLACK = 0;
+    public final int WHITE = 1;
+    public final int GREEN = 2;
+    public final int RED = 3;
+    public final int BLUE = 4;
+    public final int YELLOW = 5;
+
+    private int turn;
+    private boolean turnOver;
+    private Player[] players;
+    private boolean gameInProgress;
+    private boolean gameStarted;
+    private Card[] level1Cards;
+    private Card[] level2Cards;
 
     public void paint(Graphics g)
     {
         
     }
+
     public void mousePressed(MouseEvent e) {}
     public void mouseClicked(MouseEvent e) {}
     public void mouseEntered(MouseEvent e) {}
