7501736d546e6676adc3fdbe577976e5e6957378
fidgetspinner71 <nathans.fs71@gmail.com>
2024-11-05 08:50:46 -0600

    Creates Window

--- src/SplendorFrame.java
+++ src/SplendorFrame.java
@@ -1,4 +1,13 @@
+import javax.swing.*;
 
-public class SplendorFrame {
-
+public class SplendorFrame extends JFrame {
+    private final int WIDTH = 1280;
+    private final int HEIGHT = 720;
+    public SplendorFrame()
+    {
+        super("Splendor");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
--- src/SplendorRunner.java
+++ src/SplendorRunner.java
@@ -2,6 +2,6 @@
 public class SplendorRunner {
     public static void main(String args[])
     {
-        System.out.println("yo");
+        SplendorFrame window = new SplendorFrame();
     }
 }
