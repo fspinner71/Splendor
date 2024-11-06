739d3b4ced7ceda56c1346c457faa60a7c55edd2
jonathan117lee <k1222168@students.katyisd.org>
2024-11-06 09:26:19 -0600

    game intiliztion basics
    added base methods for intialization of the game

--- src/SplendorGame.java
+++ src/SplendorGame.java
@@ -20,12 +20,55 @@ public class SplendorGame extends JPanel implements MouseListener{
     private Card[] cards1;
     private Card[] cards2;
     private Card[] cards3;
+    private Patron[] patrons;
     private ArrayList<Card> draw1;
     private ArrayList<Card> draw2;
     private ArrayList<Card> draw3;
-
-    public SplendorGame()
+    public int[] tokens;
+    public int size;
+    public SplendorGame(int size)
     {
+        this.size = size; //how many players are playign the game
+
+        
+        makeTokens();
+        makeCards();
+        makePatrons();
+        makePlayers();
+    }
+    public void makeTokens() {
+
+        tokens = new int[6]; //initalize tokens and add 6 of each except yellow
+        for(int c  = 0; c < tokens.length-1; c++) {
+            if(size == 2) { // change token count for different player sizes
+                tokens[c] = 4;
+            }
+            if(size == 3) {
+                tokens[c] = 5;
+            }
+            if(size == 4) {
+                tokens[c] = 7;
+            }
+
+        }
+        tokens[5] =  5; //5 yellow tokens always
+
+
+    }
+    public void makePatrons() {
+        ArrayList<Patron> patronDeck = new ArrayList<Patron>(); //temporary patron deck that will contain all patrons from the csv file
+        patrons = new Patron[size+1];  //curent usable patrons
+ 
+
+        // shuffle patron deck and add the number needed to the array(varies with player size)
+    }
+
+    public void makeCards() {
+
+        //take in csv file and make the cards and add it to the arrays and arraylist piles
+    }
+    public void makePlayers() {
+        players = new Player[size];
 
     }
 
@@ -34,6 +77,11 @@ public class SplendorGame extends JPanel implements MouseListener{
         super.paint(g);
         g.setColor(Color.black);
         g.drawString("HELLO", 600, 300);
+    }
+
+    public void createCards() {
+
+
     }
 
     public void mousePressed(MouseEvent e) {}
