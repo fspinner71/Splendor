d0a47eac46a5d72d778a5f98e47efaa3a5c5a616
jonathan117lee <k1222168@students.katyisd.org>
2024-11-20 09:06:48 -0600

    a

--- src/SplendorGame.java
+++ src/SplendorGame.java
@@ -291,7 +291,7 @@ repaint();
         draw2.remove(4);
 
     }
-
+    
     public void makeLevel3() {
         String line;
         cards3 = new Card[4]; //create stuff
@@ -628,6 +628,26 @@ repaint();
         }
     }
 
+<<<<<<< Updated upstream
+=======
+
+    public void nextTurn() { //turn moves 0-3  //buy patron too
+
+        if(turn == players.length-1) {
+            turn = 0;
+        }
+        else {
+            turn++;
+        }
+        tokenClickCount = new int[6]; //reset token vairalbes so next player can click stuff
+        tokenClickCounter = 0;
+        canClickMoreTokens = true;
+        canBuyCard = false;
+
+
+    }
+
+>>>>>>> Stashed changes
     public void errorScreen(){
 
     }
@@ -654,6 +674,8 @@ repaint();
                     if (tokenClickCount[i] == 2) errorScreen();
                 }
             }
+
+            tokenClickCounter++;
         }
 
         if (tokenButtons[5].isInside(x, y)){ // Golden tokens, WIP
